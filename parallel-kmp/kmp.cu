c68ddbac32a71c87705d34f2c5a274a595fb3b89
YingZong887 <yz887@cornell.edu>
2017-05-08 18:40:22 -0400

    add output to kmp cuda

--- parallel-kmp/kmp.cu
+++ parallel-kmp/kmp.cu
@@ -3,97 +3,97 @@
 #include <string.h>
 #include <time.h>
 
-
-void preKMP(char* pattern, int f[])
-{
-    int m = strlen(pattern), k;
-    f[0] = -1;
-    for (int i = 1; i < m; i++)
-    {
-        k = f[i - 1];
-        while (k >= 0)
-        {
-            if (pattern[k] == pattern[i - 1])
-                break;
-            else
-                k = f[k];
+// build the kmp table for the subsequent operations
+void preKMP(char* pattern, int func[]) {
+    int m = strlen(pattern);
+    int k;
+    func[0] = -1;
+    for (int i = 1; i < m; i++) {
+        k = func[i - 1];
+        while (k >= 0) {
+            if (pattern[k] == pattern[i - 1]) {
+                 break;
+            }
+            else {
+                k = func[k];
+            }
         }
-        f[i] = k + 1;
+        func[i] = k + 1;
     }
 }
 
-//check whether target string contains pattern 
-__global__ void KMP(char* pattern, char* target,int f[],int c[],int n, int m)
-{
-    int index = blockIdx.x*blockDim.x + threadIdx.x;
-    int i = n * index;
-    int j = n * (index + 2)-1;
-    if(i>m)
+// Kernel function. Implement the KMP algorithm
+__global__ void KMP(char* pattern, char* target, int func[], int answer[], int pattern_length, int target_length) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = pattern_length * index;
+    int j = pattern_length * (index + 2) - 1;
+    
+    if(i > target_length) {
         return;
-    if(j>m)
-        j=m;
+    }
+
+    if(j > target_length) {
+        j = target_length;
+    }
+
     int k = 0;        
-    while (i < j)
-    {
-        if (k == -1)
-        {
+    while (i < j) {
+        if (k == -1) {
             i++;
             k = 0;
-        }
-        else if (target[i] == pattern[k])
-        {
+        } else if (target[i] == pattern[k]) {
             i++;
             k++;
-            if (k == n)
-            {
-                c[i - n] = i-n;
+            if (k == pattern_length) {
+                answer[i - pattern_length] = i - pattern_length;
                 i = i - k + 1;
             }
         }
-        else
-            k = f[k];
+        else {
+            k = func[k];
+        }
     }
     return;
 }
 
-int main(int argc, char* argv[])
-{
-    const int L = 260;
-    const int S = 4;
-    int M = 4;//num of threads
+int main(int argc, char* argv[]) {
+    const int target_length = 260;
+    const int pattern_length = 4;
+    int M = 4;
 
-    char *tar;
-    char *pat;
-    tar = (char*)malloc(L*sizeof(char));
-    pat = (char*)malloc(S*sizeof(char));
+    char *target;
+    char *pattern;
+    target = (char*)malloc(target_length * sizeof(char));
+    pattern = (char*)malloc(pattern_length * sizeof(char));
 
-    char* b="abcdefghijklmnopqrstuvwxyz";
-    for (int i = 0; i < L; i++) {
-        tar[i] = b[i%26];
+    char* dict = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < target_length; i++) {
+        target[i] = dict[i%26];
     }
-    for (int j=0; j < S; j++) {
-        pat[j] = b[j];
+    for (int j = 0; j < pattern_length; j++) {
+        pattern[j] = dict[j%26];
     }
-    char *d_tar;
-    char *d_pat;
+    char *d_target;
+    char *d_pattern;
 
-    int m = strlen(tar);
-    int n = strlen(pat);
-    printf("%d %d\n",m,n);
-    int *f;
-    int *c;
+    printf("The target length is: %d, the pattern length is %d.\n", target_length, pattern_length);
+    printf("The target string is: \n");
+    printf("%s\n", target);
+    printf("The pattern string is: \n");
+    printf("%s\n", pattern);
+    int *func;
+    int *answer;
 
-    f = new int[m];
-    c = new int[m];
+    func = new int[target_length];
+    answer = new int[target_length];
 
-    int *d_f;
-    int *d_c;
-    for(int i = 0;i<m; i++)
-    {
-        c[i] = -1;
+    int *d_func;
+    int *d_answer;
+    for(int i = 0; i < target_length; i++) {
+        answer[i] = -1;
     }     
 
-    preKMP(pat, f);
+    preKMP(pattern, func);
 
     cudaEvent_t start, stop;
     float elapsedTime;
@@ -103,19 +103,19 @@ int main(int argc, char* argv[])
 
     cudaEventRecord( start, 0 );
 
-    cudaMalloc((void **)&d_tar, m*sizeof(char));
-    cudaMalloc((void **)&d_pat, n*sizeof(char));
-    cudaMalloc((void **)&d_f, m*sizeof(int));
-    cudaMalloc((void **)&d_c, m*sizeof(int));
+    cudaMalloc((void **)&d_target, target_length * sizeof(char));
+    cudaMalloc((void **)&d_pattern, pattern_length * sizeof(char));
+    cudaMalloc((void **)&d_func, target_length * sizeof(int));
+    cudaMalloc((void **)&d_answer, target_length * sizeof(int));
 
-    cudaMemcpy(d_tar, tar, m*sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_pat, pat, n*sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_f, f, m*sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_c, c, m*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_target, target, target_length * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_pattern, pattern, pattern_length * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_func, func, target_length * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_answer, answer, target_length * sizeof(int), cudaMemcpyHostToDevice);
 
-    KMP<<<(m/n+M)/M,M>>>(d_pat, d_tar ,d_f, d_c, n, m);
+    KMP<<<(target_length / pattern_length + M)/M, M>>>(d_pattern, d_target ,d_func, d_answer, pattern_length, target_length);
 
-    cudaMemcpy(c, d_c, m*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(answer, d_answer, target_length * sizeof(int), cudaMemcpyDeviceToHost);
 
     cudaEventRecord( stop, 0 );
     cudaEventSynchronize( stop );
@@ -123,12 +123,18 @@ int main(int argc, char* argv[])
     cudaEventDestroy(start); 
     cudaEventDestroy(stop);
 
-    printf("When the target length is %d, pattern length is %d, the elapsed time is %0.3f.\n ms", m, n, elapsedTime); 
+    printf("When the target length is %d, pattern length is %d, the elapsed time is %0.3f ms.\n", target_length, pattern_length, elapsedTime); 
 
-
-    for(int i = 0; i < m; i++) {
-        //printf("%d\n", c[i]);
+    for(int i = 0; i < target_length; i++) {
+        if (answer[i] != -1) {
+            printf("Find a matching substring starting at: %d.\n", i);
+        }
     }
-    cudaFree(d_tar); cudaFree(d_pat); cudaFree(d_f); cudaFree(d_c);
+
+    cudaFree(d_target); 
+    cudaFree(d_pattern); 
+    cudaFree(d_func); 
+    cudaFree(d_answer);
+
     return 0;
-}}
+}
